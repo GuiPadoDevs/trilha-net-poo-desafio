2987d69f74ecdffef6dd0fdd12ee0e4d752877ea
GuiPadoDevs <guilherme.padovine@wemov.com.br>
2024-08-03 09:27:26 -0300

    Feito desafio POO da DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("123456", "Nokia 3310", "111111111111111", 16);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        Smartphone iphone = new Iphone("654321", "iPhone 12", "222222222222222", 64);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
